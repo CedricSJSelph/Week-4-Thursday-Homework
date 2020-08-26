fff58cfa17158615bf235943950874e6e126dcf2
koastiebratt@gmail.com <you@example.com>
2020-08-26 11:40:17 -0400

    That was tough...

--- src/EliminateDuplicates.java
+++ src/EliminateDuplicates.java
@@ -1,9 +1,60 @@
+import java.util.Scanner;
 
 public class EliminateDuplicates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		int list[] = new int[10];
+		
+		System.out.print("Please Enter 10 numbers: ");
+		
+		for(int z = 0; z < list.length; z++) {
+			
+			list[z] = input.nextInt();
+		}
+		
+		int New_List[] = eliminateDuplicates(list);
+		
+		System.out.print("Your List without Duplicates is: ");
+		
+		for(int l = 0; l < New_List.length; l++) {
+			
+			
+			
+			if(New_List[l] != 0) {
+				
+				
+				System.out.print(New_List[l] + " ");
+			}
+			
+		}
 
 	}
-
+	
+	public static int[] eliminateDuplicates(int[] list) {
+		
+		java.util.Arrays.sort(list);
+		
+		//Checks each position to the rest
+		for (int i = 0; i < list.length; i++){
+			
+			//Compares to rest
+		    for (int j = 1; j < list.length; j++) {
+		    	
+		    	if(i != j && list[i] == list[j]) {
+		                	
+		               list[j] = 0;
+		              
+		    	}
+		    }
+		}
+		
+		return list;
+		          
+		                   
+	}
 }
+
+	
