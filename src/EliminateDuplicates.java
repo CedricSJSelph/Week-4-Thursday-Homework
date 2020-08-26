7e94aaa8a35ee7e74fdc34400a44c14f410abeef
koastiebratt@gmail.com <you@example.com>
2020-08-26 12:31:58 -0400

    Problems

--- src/EliminateDuplicates.java
+++ src/EliminateDuplicates.java
@@ -19,12 +19,16 @@ public class EliminateDuplicates {
 		
 		System.out.print("Your List without Duplicates is: ");
 		
+		if(New_List[0] == 0) {
+			
+			System.out.print(0 + " ");
+			
+		}
+		
 		for(int l = 0; l < New_List.length; l++) {
 			
-			
-			
+			//Duplicates are now zero, irrelevant
 			if(New_List[l] != 0) {
-				
 				
 				System.out.print(New_List[l] + " ");
 			}
@@ -37,23 +41,22 @@ public class EliminateDuplicates {
 		
 		java.util.Arrays.sort(list);
 		
-		//Checks each position to the rest
+		int New_List[] = new int[10];;
+	
+		//Checks each position individually
 		for (int i = 0; i < list.length; i++){
 			
-			//Compares to rest
+			//Compares to rest 
 		    for (int j = 1; j < list.length; j++) {
 		    	
-		    	if(i != j && list[i] == list[j]) {
-		                	
-		               list[j] = 0;
-		              
+		    	if(list[i] != list[j]) {
+		    		
+		    		System.out.print(list[i]);
 		    	}
+		    	
 		    }
 		}
-		
-		return list;
-		          
-		                   
+		return New_List;                   
 	}
 }
 
